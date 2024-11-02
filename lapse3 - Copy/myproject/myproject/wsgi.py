28d17a5a6a728c225e1ac5bede261d38ae134296
Ajeem shaik <132434412+ajeemshaik7@users.noreply.github.com>
2024-11-02 21:09:37 +0530

    Update wsgi.py

--- lapse3 - Copy/myproject/myproject/wsgi.py
+++ lapse3 - Copy/myproject/myproject/wsgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+# Get the WSGI application.
 application = get_wsgi_application()
 
-app = application
+
